8ca27ac8f7b1534bbe0263472a7c07ee25f9f851
akarapol.r <akarapol.r@codium.co>
2020-08-05 14:16:55 +0700

    solved solution 2

--- 2.immutability.ts
+++ 2.immutability.ts
@@ -5,13 +5,15 @@ The function below is a "safe divide" function. That is, it promises not to thro
 
 Questions:
 1) Spot and describe the cause of the bug that causes the "divide by zero" to occur.
+    เพราะว่า let นั้นสามารถเปลี่ยนแปลงค่าได้แต่ว่าการกำหนด const นั้นไม่ยอมให้ค่าเปลี่ยนแปลง
 2) Now, make a *single* code change on line 14:
 2.1) without changing the number 42;
 2.2) such that cause of the bug can be spotted by the compiler at *compile time*.
 3) What did you learn from this exercise?
+    Immutability คือการที่ไม่ต้องเปลี่ยนแปลงค่า
 */
 function safeDivide(): number {
-    let x: number = 42;
+    const x: number = 42;
 
     if (x == 0) throw new Error("x should not be 0");
 
