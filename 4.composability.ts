4789c584af735543d6484a695abb7613e760c6ff
akarapol.r <akarapol.r@codium.co>
2020-08-05 14:17:07 +0700

    solved solution 4

--- 4.composability.ts
+++ 4.composability.ts
@@ -8,16 +8,16 @@ explaining your understanding is what's required.
 //
 // Non-composable implementation
 //
-function findFirstNegativeNumberNonComposable(numbers: number[]) : number | undefined {
+function findFirstNegativeNumberNonComposable(numbers: number[]): number | undefined {
     for (let i of numbers) {
-        if (i < 0) 
-        return i;
+        if (i < 0)
+            return i;
     }
 }
-function findFirstOneCharacterStringNonComposable(strings: string[]) : string | undefined {
+function findFirstOneCharacterStringNonComposable(strings: string[]): string | undefined {
     for (let str of strings) {
         if (str.length == 1)
-         return str; 
+            return str;
     }
 }
 
@@ -25,17 +25,17 @@ function findFirstOneCharacterStringNonComposable(strings: string[]) : string | 
 Suppose now you want to log an error whenever we fail to find an element. The Non-composable
 implementation now with logging probably will look like this.
 */
-function findFirstNegativeNumberNonComposableWithLogging(numbers: number[]) : number | undefined {
+function findFirstNegativeNumberNonComposableWithLogging(numbers: number[]): number | undefined {
     for (let i of numbers) {
-        if (i < 0) 
-        return i;
+        if (i < 0)
+            return i;
     }
     console.error("No matching value found");
 }
-function findFirstOneCharacterStringNonComposableWithLogging(strings: string[]) : string | undefined {
+function findFirstOneCharacterStringNonComposableWithLogging(strings: string[]): string | undefined {
     for (let str of strings) {
         if (str.length == 1)
-         return str; 
+            return str;
     }
     console.error("No matching value found");
 }
@@ -47,17 +47,17 @@ Now, I refactor the code such that it looks like this now.
 //
 // Composable implementation
 //
-function first<T>(range: T[], p: (elem: T) => boolean) : T | undefined {
+function first<T>(range: T[], p: (elem: T) => boolean): T | undefined {
     for (let elem of range) {
-        if (p (elem)) 
-        return elem;
+        if (p(elem))
+            return elem;
     }
     console.error("No matching value found");
 }
-function findFirstNegativeNumberComposable(numbers: number[]) : number | undefined {
+function findFirstNegativeNumberComposable(numbers: number[]): number | undefined {
     return first(numbers, n => n < 0);
 }
-function findFirstOneCharacterStringComposable(strings: string[]) : string | undefined {
+function findFirstOneCharacterStringComposable(strings: string[]): string | undefined {
     return first(strings, str => str.length == 1);
 }
 
@@ -69,4 +69,6 @@ Think. Compare and contrast the:
 Question:
 1) What did you learn from this problem? Why would we want to refactor the code to be
 like the Composable implementation?
+    การยุบรวบของโค้ดที่ซ้ำซ้อนเพื่อที่จะทำให้โค้ดมีความcleanเมื่อมีการเขียนโค้ดที่ซ้ำซ้อนกันควรจะมีการComposable เพื่อที่จะได้เรียกใช้เพียงครั้งเดียวเมื่อต้องการ
+
 */
